3dfb44f85b8a216dc8e281c2b83bd99a1c6cba02
Saranya-dev-05 <76162789+Saranya-dev-05@users.noreply.github.com>
2021-02-02 12:37:54 +0530

    Update index5.blade.php

--- resources/views/index5.blade.php
+++ resources/views/index5.blade.php
@@ -1281,6 +1281,16 @@
                   <!--/ Count Card End -->
                   <div class="row row-xs clearfix">
                      <!--================================-->
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                 
                      <!-- Latest Projects Start-->
                      <!--================================-->
                      <div class="col-lg-12 col-xl-8">
@@ -1311,22 +1321,6 @@
                                     </thead>
                                     <tbody>
                                        <tr>
-                                          <td><span >01</span></td>
-                                          <td><a href="" class="text-inherit">#MN67GNF6</a></td>
-                                          <td>Maria Grant</td>
-                                          <td>
-                                             <div class="clearfix">
-                                                <div class="float-left"> <strong>67%</strong> </div>
-                                                <div class="float-right"> <small class="tx-gray-500">Mar 01, 2018 - Sep 30, 2019</small> </div>
-                                             </div>
-                                             <div class="progress ht-3 op-5">
-                                                <div class="progress-bar bg-cyan" role="progressbar" style="width: 67%" aria-valuenow="67" aria-valuemin="0" aria-valuemax="100"></div>
-                                             </div>
-                                          </td>
-                                          <td>$26,908</td>
-                                          <td class="text-right"> <a href="" class="btn btn-label-primary btn-sm mg-y-5"><i class="fa fa-pencil"></i> Edit</a> <a href="" class="btn btn-label-success btn-sm mg-y-5"><i class="fa fa-link"></i> Add</a> <a href="" class="btn btn-label-danger btn-sm mg-y-5"><i class="fa fa-trash"></i> Delete</a> </td>
-                                       </tr>
-                                       <tr>
                                           <td><span >02</span></td>
                                           <td><a href="" class="text-inherit">#4FGB65GN</a></td>
                                           <td>Stewart Bower</td>
@@ -1342,54 +1336,6 @@
                                           <td>$15,987</td>
                                           <td class="text-right"> <a href="" class="btn btn-label-primary btn-sm mg-y-5"><i class="fa fa-pencil"></i> Edit</a> <a href="{{ url('add') }}" class="btn btn-label-success btn-sm mg-y-5"><i class="fa fa-link"></i> Add</a> <a href="" class="btn btn-label-danger btn-sm mg-y-5"><i class="fa fa-trash"></i> Delete</a> </td>
                                        </tr>
-                                       <tr>
-                                          <td><span >03</span></td>
-                                          <td><a href="" class="text-inherit">#RGB654NH</a></td>
-                                          <td>Kevin Gill</td>
-                                          <td>
-                                             <div class="clearfix">
-                                                <div class="float-left"> <strong>17%</strong> </div>
-                                                <div class="float-right"> <small class="tx-gray-500">Jun 09, 2018 - Oct 30, 2019</small> </div>
-                                             </div>
-                                             <div class="progress ht-3 op-5">
-                                                <div class="progress-bar bg-danger" role="progressbar" style="width: 17%" aria-valuenow="17" aria-valuemin="0" aria-valuemax="100"></div>
-                                             </div>
-                                          </td>
-                                          <td>$12,456</td>
-                                          <td class="text-right"> <a href="" class="btn btn-label-primary btn-sm mg-y-5"><i class="fa fa-pencil"></i> Edit</a> <a href="" class="btn btn-label-success btn-sm mg-y-5"><i class="fa fa-link"></i> Add</a> <a href="" class="btn btn-label-danger btn-sm mg-y-5"><i class="fa fa-trash"></i> Delete</a> </td>
-                                       </tr>
-                                       <tr>
-                                          <td><span >04</span></td>
-                                          <td><a href="" class="text-inherit">#3HBN547R</a></td>
-                                          <td>Johnsie Arel</td>
-                                          <td>
-                                             <div class="clearfix">
-                                                <div class="float-left"> <strong>30%</strong> </div>
-                                                <div class="float-right"> <small class="tx-gray-500">July 13, 2018 - Nov 20, 2019</small> </div>
-                                             </div>
-                                             <div class="progress ht-3 op-5">
-                                                <div class="progress-bar bg-warning" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-                                             </div>
-                                          </td>
-                                          <td>$17,376</td>
-                                          <td class="text-right text-white"> <a href="" class="btn btn-label-primary btn-sm mg-y-5"><i class="fa fa-pencil"></i> Edit</a> <a href="" class="btn btn-label-success btn-sm mg-y-5"><i class="fa fa-link"></i> Add</a> <a href="" class="btn btn-label-danger btn-sm mg-y-5"><i class="fa fa-trash"></i> Delete</a> </td>
-                                       </tr>
-                                       <tr>
-                                          <td><span >05</span></td>
-                                          <td><a href="" class="text-inherit">#KR346GSH</a></td>
-                                          <td>Wallace Aliff</td>
-                                          <td>
-                                             <div class="clearfix">
-                                                <div class="float-left"> <strong>50%</strong> </div>
-                                                <div class="float-right"> <small class="tx-gray-500">Aug 05, 2018 - Dec 21, 2019</small> </div>
-                                             </div>
-                                             <div class="progress ht-3 op-5">
-                                                <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                             </div>
-                                          </td>
-                                          <td>$23,876</td>
-                                          <td class="text-right"> <a href="" class="btn btn-label-primary btn-sm mg-y-5"><i class="fa fa-pencil"></i> Edit</a> <a href="" class="btn btn-label-success btn-sm mg-y-5"><i class="fa fa-link"></i> Add</a> <a href="" class="btn btn-label-danger btn-sm mg-y-5"><i class="fa fa-trash"></i> Delete</a> </td>
-                                       </tr>
                                     </tbody>
                                  </table>
                               </div>
@@ -1397,6 +1343,23 @@
                         </div>
                      </div>
                      <!--/ Latest Projects End -->
+                     
+                     
+                     
+                     
+            
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
+                     
                      <!--================================-->
                      <!-- Project By Country Start-->
                      <!--================================-->
